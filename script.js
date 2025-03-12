37ed7aaf5a3effba2df8fae2e7db0e435172921a
Kingfurious <akhilmanikandannair86@gmail.com>
2025-03-12 14:19:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// scripts.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill out all fields.');
+        event.preventDefault();
+    }
+});
